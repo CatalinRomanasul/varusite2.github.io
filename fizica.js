c342b316e783115b53ab530147ed0ca4cb11b004
CatalinRomanasul <catalinmocanu116@gmail.com>
2024-11-26 05:21:18 -0800

    Update fizica.js

--- fizica.js
+++ fizica.js
@@ -2,13 +2,15 @@ const gridContainer1 = document.querySelector('.js-grid-items-container1');
 const gridContainer2 = document.querySelector('.js-grid-items-container2');
 const gridContainer3 = document.querySelector('.js-grid-items-container3');
 const Button = document.querySelector('.js-button');
+const buttonSubject1  = document.querySelector('.button-set1');
+const buttonSubject2  = document.querySelector('.button-set2');
+const buttonSubject3  = document.querySelector('.button-set3');
+
 
 for(let i = 1 ; i<101;i++)
-  {gridContainer1.innerHTML+=`<a class="grid-items light-mode" target="_blank" href="e_f_termodinamica_si_00${i}.pdf">Fisa ${i}</a>`;}
-
-for(let i = 1 ; i<101;i++)
-  {gridContainer2.innerHTML+=`<a class="grid-items light-mode" target="_blank" href="e_f_termodinamica_sii_00${i}.pdf">Fisa ${i}</a>`;}
-
+  {gridContainer2.innerHTML+=
+`<div><a class="grid-items light-mode" target="_blank" href="e_f_termodinamica_sii_00${i}.pdf">Fisa ${i}</a></div>`
+}
 
 for(let i = 1 ; i<101;i++)
   {gridContainer3.innerHTML+=`<a class="grid-items light-mode" target="_blank" href="e_f_termodinamica_siii_00${i}.pdf">Fisa ${i}</a>`;}
@@ -16,6 +18,12 @@ for(let i = 1 ; i<101;i++)
 
 Button.addEventListener('click',()=>{
   darkMode();
+})
+
+document.querySelectorAll('.js-choose').forEach((button,index)=>{
+  button.addEventListener('click',()=>{
+ displaySet(index);
+  })
 })
 
 
@@ -37,4 +45,33 @@ function darkMode(){
   document.body.classList.remove('dark-mode');
   }}
 
+
+
+
+
+
+
+
+
+
+
+
+function ReturnNumOfI(nrI){
+let numI='i';
+let stringNums='';
+while(nrI>0){
+  
+  stringNums+=numI;
+  nrI--;
+}
+return stringNums;
+}
+
+
+function displaySet(index){
+
  
+    gridContainer1.innerHTML=`<span>Setul ${index+1}</span>`;
+    for(let i = 1 ; i<101;i++)
+    {gridContainer1.innerHTML+=`<a class="grid-items light-mode" target="_blank" href="e_f_termodinamica_s${ReturnNumOfI(index+1)}_00${i}.pdf">Fisa ${i}</a>`;}
+}
